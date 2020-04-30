64f9423eb3b8f4e2d38250f384235759681b4025
Alejandro Villegas <neopilatos@gmail.com>
2020-04-29 19:28:05 -0500

    Second tab

--- comprayrecoje/index.js
+++ comprayrecoje/index.js
@@ -1,5 +1,5 @@
 
-function openCity(evt, cityName) {
+function openTienda(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -19,4 +19,26 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+function opentypesOfProducts(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent2, tablinks;
+
+  // Get all elements with class="tabcontent2" and hide them
+  tabcontent2 = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent2.length; i++) {
+    tabcontent2[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpenProducts").click();
